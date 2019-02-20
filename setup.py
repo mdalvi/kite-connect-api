7aac692716c86b317fc23ad3ee8964c5949b5656
Milind Dalvi <milind.dalvi14@gmail.com>
2019-02-20 09:43:21 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='kite_connect_api',
+    version='0.1',
+    packages=['kite_connect_api'],
+    url='https://turingequations.com',
+    requires=['kiteconnect'],
+    license='GNU GENERAL PUBLIC LICENSE v3.0',
+    author='mdalvi',
+    author_email='milind.dalvi@turingequations.com',
+    description='This is a wrapper for https://github.com/zerodhatech/pykiteconnect'
+)
