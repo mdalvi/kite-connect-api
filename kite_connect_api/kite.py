4a72c7652d841ff868e27de5edcd47e2af3d5ae8
Milind Dalvi <milind.dalvi14@gmail.com>
2019-02-21 23:46:16 +0530

    place_order time difference optimized

--- kite_connect_api/kite.py
+++ kite_connect_api/kite.py
@@ -10,27 +10,34 @@ class Kite(object):
     def __init__(self, **kite_credentials):
         self.kite = KiteConnect(**kite_credentials, debug=True)
         self.sw_place_order = time.time()
+        self.nb_place_order = 0
 
     def exec(self, func_name, *args, **kwargs):
         logger = logging.getLogger(__name__)
+        response = None
         while True:
             try:
                 if func_name == 'place_order':
-                    if (time.time() - self.sw_place_order) < 0.2:
-                        time.sleep(max(0.0, 0.2 - (time.time() - self.sw_place_order)))
-                    self.sw_place_order = time.time()
+                    if self.nb_place_order >= 5:
+                        if (time.time() - self.sw_place_order) < 1.0:
+                            time.sleep(max(0.0, 1.0 - (time.time() - self.sw_place_order)))
+                        self.nb_place_order = 0
 
-                return getattr(self.kite, func_name)(*args, **kwargs)
+                    response = getattr(self.kite, func_name)(*args, **kwargs)
+                    if self.nb_place_order == 0:
+                        self.sw_place_order = time.time()
+                    self.nb_place_order += 1
+                else:
+                    response = getattr(self.kite, func_name)(*args, **kwargs)
+                break
             except ConnectionError as ex:
                 logger.warning("requests.exceptions.ConnectionError @ [{}]".format(repr(ex)))
-                time.sleep(0.1)
             except Timeout as ex:
                 logger.warning("requests.exceptions.Timeout @ [{}]".format(repr(ex)))
-                time.sleep(0.1)
             except NetworkException as ex:
                 logger.warning("kiteconnect.exceptions.NetworkException @ [{}]".format(repr(ex)))
                 time.sleep(1)
             except KiteException as ex:
                 logger.warning("kiteconnect.exceptions.KiteException @ [{}]".format(repr(ex)))
                 break
-        return None
+        return response
